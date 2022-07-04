31726f3ccfea1551fcdd073a4140004a2fbcb2ae
payel <payel.nandy2580@gmail.com>
2022-07-04 14:34:59 -0400

    Table

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,8 +1,27 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import java.util.Scanner;
+
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+//        int a;
+//        System.out.println("Enter no. ");
+//        Scanner in = new Scanner(System.in);
+//        a = in.nextInt();
+
+
+        String finalResult="";
+        String result="";
+        for(int j = 1; j <= 5; j++){
+            for(int i = 1; i <= 5; i++){
+                result = result + " " +(j*i)+ " |" ;
+            }
+//            System.out.println(result);
+            finalResult = finalResult+result+ "\n";
+            result="";
+        }
+        System.out.println(finalResult);
+        return finalResult;
     }
 
     public static String getLargeMultiplicationTable() {
